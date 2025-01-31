a6491a60a19be5489ea864df2339bbdb474793e4
Bert Kramer <info@e-chef.nl>
2025-01-31 20:43:46 +0100

    req2

--- app.py
+++ app.py
@@ -2423,8 +2423,8 @@ def terms():
 # Start de server
 # -----------------------------------------------------------
 if __name__ == '__main__':
-    app.run(debug=True)
-
-
+    port = int(os.environ.get('PORT', 5000))
+    debug_mode = os.environ.get('FLASK_ENV') == 'development'
+    app.run(host='0.0.0.0', port=port, debug=debug_mode)
 
 
