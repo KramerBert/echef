757a2fc78d169dc1aacb03de8e104f493c6ab241
Bert Kramer <info@e-chef.nl>
2025-02-02 13:15:41 +0100

    Fix route for quickstart

--- blueprints/main/routes.py
+++ blueprints/main/routes.py
@@ -1,9 +1,9 @@
 from flask import Blueprint, render_template
 from flask_wtf import FlaskForm
 
-main = Blueprint('main', __name__, url_prefix='')
+main = Blueprint('main', __name__)
 
 @main.route('/quickstart')
-def quickstart():
+def quickstart():    # Deze functienaam wordt gebruikt in url_for()
     """Show the quickstart guide page"""
     return render_template('main/quickstart.html', form=FlaskForm())
